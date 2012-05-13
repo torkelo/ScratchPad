fc9e104c225ee4ca2ed6f1de24479c8d84264f66
= <=>
2012-05-13 17:40:20 +0200

    hm.. validation??

--- BBoneTrader/BBoneTrader.Web/Public/js/NewAuction/BBoneTrader.NewAuction.js
+++ BBoneTrader/BBoneTrader.Web/Public/js/NewAuction/BBoneTrader.NewAuction.js
@@ -9,16 +9,18 @@ BBoneTrader.NewAuction = function (Backbone, $) {
     var NewAuctionView = BBoneTrader.View.extend({
         template: "#new-auction-template",
         events: {
-            "click #create-auction": "create"
+            "click #create-auction": "create",
+            "change input,textarea": "fieldChange"
+        },
+
+        fieldChange: function(e) {
+            var field = $(e.currentTarget);
+            var data = {};
+            data[field.attr('id')] = field.val();
+            this.model.set(data);            
         },
 
         create: function() {
-            this.model.set({
-                Title: $("#auction-title").val(),
-                Description: $("#auction-description").val(),
-                MinBid: $("#auction-min-bid").val()
-            });
-
             this.model.save();
         }
 
